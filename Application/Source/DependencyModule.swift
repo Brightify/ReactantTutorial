d9bdb9314c2fa9f936196ae2dfea572feb95c05f
Tadeas Kriz <tadeas@brightify.org>
2017-05-02 00:57:40 +0200

    Missed issues from tutorial.

--- Application/Source/DependencyModule.swift
+++ Application/Source/DependencyModule.swift
@@ -1,7 +1,4 @@
 // DependencyModule.swift
 protocol DependencyModule {
-    var userService: UserService { get }
-    var anotherService: AnotherService { get }
-
     func greeterService(language: GreeterService.Language) -> GreeterService
 }
